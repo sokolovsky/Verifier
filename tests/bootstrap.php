808a625b4fdfebcacc051a71c0c10ee7322f620d
Max Sokolovsky <sokolovsky@worksolutions.ru>
2015-03-27 18:14:01 +0300

    Tests. Reconfigurating bootstrap file.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,14 @@
 <?php
-require_once __DIR__.'/../vendor/autoload.php';
+$directory = __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'src';
+
+include_once $directory.DIRECTORY_SEPARATOR.'Conditions.php';
+include_once $directory.DIRECTORY_SEPARATOR.'Exceptions.php';
+
+spl_autoload_register(function ($class) use ($directory) {
+    if (!in_array(strpos($class, 'Verifier'), array(0, 1), true)) {
+        return false;
+    }
+    $class = ltrim($class, '\\');
+    $file = preg_replace('/^Verifier\\\\/', '', $class).'.php';
+    include_once $directory.DIRECTORY_SEPARATOR.$file;
+});
